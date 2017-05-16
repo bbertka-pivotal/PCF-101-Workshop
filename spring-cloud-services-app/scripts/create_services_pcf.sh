6fcaa56517c561a526dedc024c686bbe7496245a
Brendan <brendanaye@gmail.com>
2017-05-16 15:47:11 -0700

    Update create_services_pcf.sh

--- spring-cloud-services-app/scripts/create_services_pcf.sh
+++ spring-cloud-services-app/scripts/create_services_pcf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf cs p-mysql 100mb-dev fortunes-db
-cf create-service p-config-server standard config-server -c '{"git": { "uri": "https://github.com/bbertka-pivotal/PCF-101-Workshop", "searchPaths": "spring-cloud-services-app/configuration" } }'
+cf create-service p-config-server standard config-service -c '{"git": { "uri": "https://github.com/bbertka-pivotal/PCF-101-Workshop", "searchPaths": "spring-cloud-services-app/configuration" } }'
 cf cs p-service-registry standard service-registry
 cf cs p-circuit-breaker-dashboard standard circuit-breaker
